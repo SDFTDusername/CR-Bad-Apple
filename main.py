85d7373bf5ad0ec3ed4236faaf0b70fd58e02925
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2024-12-27 12:54:40 +0100

    Added support for multiple signs

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ Frame = typing.List[FrameLine]
 Frames = typing.List[Frame]
 
 colors = ("#", "[.", "+", "-", "*.", ". ", "  ")
-resolution = (15, 4)
+sign_resolution = (15, 4)
+block_resolution = (1, 1)
 
 video_file = "video.mp4"
 audio_file = "audio.mp3"
@@ -28,7 +29,9 @@ start_keybind = "k"
 stop_keybind = 'j'
 
 delay = 0.1
+mouse_move_amount = 100
 
+video_resolution = (sign_resolution[0] * block_resolution[0], sign_resolution[1] * block_resolution[1])
 colors_len = len(colors)
 fps = 0
 
@@ -53,17 +56,16 @@ def get_frames(file: str) -> Frames:
 
     video = cv2.VideoCapture(file)
     fps = video.get(cv2.CAP_PROP_FPS)
-
     frames = []
     success, frame = video.read()
 
     while success:
-        converted_frame = cv2.cvtColor(cv2.resize(frame, resolution), cv2.COLOR_BGR2GRAY)
+        converted_frame = cv2.cvtColor(cv2.resize(frame, video_resolution), cv2.COLOR_BGR2GRAY)
 
         new_frame = []
-        for y in range(resolution[1]):
+        for y in range(video_resolution[1]):
             new_line = []
-            for x in range(resolution[0]):
+            for x in range(video_resolution[0]):
                 new_line.append(converted_frame[y, x])
             new_frame.append(new_line)
 
@@ -75,18 +77,21 @@ def get_frames(file: str) -> Frames:
 
 def random_frame() -> Frame:
     frame = []
-    for y in range(resolution[1]):
+    for y in range(video_resolution[1]):
         line = []
-        for x in range(resolution[0]):
+        for x in range(video_resolution[0]):
             line.append(random.randint(0, 255))
         frame.append(line)
     return frame
 
 def get_line(frame_line: FrameLine) -> str:
     line = ""
-    for x in range(resolution[0]):
+    for x in range(video_resolution[0]):
         line += get_color(frame_line[x])
     return line
+
+def move_mouse_relative(x: float, y: float):
+    mouse._os_mouse.move_relative(x, y)
 
 def start(frames: Frames):
     if os.path.isdir(screenshots_dir):
@@ -98,28 +103,46 @@ def start(frames: Frames):
 
     for frame_count in range(total_frames):
         viewed_frame = frame_count + 1
-
-        if frame_count > 0:
-            # Break previous sign and wait for game to update
-            mouse.click(mouse.LEFT)
-            time.sleep(delay)
-
-        # Place sign and wait for game to update
-        mouse.click(mouse.RIGHT)
-        time.sleep(delay)
-
-        # Get frame
         frame = frames[frame_count]
 
-        # Type frame
-        for y in range(resolution[1]):
-            keyboard.write(get_line(frame[y]))
-            if y < resolution[1] - 1:
-                keyboard.press("enter")
+        last_movement = (0, 0)
 
-        # Exit screen and wait for game to update
-        keyboard.press("esc")
-        time.sleep(delay)
+        for block_y in range(block_resolution[1]):
+            abs_y = block_y * sign_resolution[1]
+            move_y = block_y * 2 - block_resolution[1] / 2
+            for block_x in range(block_resolution[0]):
+                abs_x = block_x * sign_resolution[0]
+                move_x = block_x * 2 - block_resolution[0] / 2
+
+                if block_resolution != (1, 1):
+                    new_movement = (move_x * mouse_move_amount, move_y * mouse_move_amount)
+                    move_mouse_relative(-last_movement[0] + new_movement[0], -last_movement[1] + new_movement[1])
+                    last_movement = new_movement
+                    time.sleep(delay)
+
+                if frame_count > 0:
+                    # Break previous sign and wait for game to update
+                    mouse.click(mouse.LEFT)
+                    time.sleep(delay)
+
+                # Place sign and wait for game to update
+                mouse.click(mouse.RIGHT)
+                time.sleep(delay)
+
+                # Type frame
+                for y in range(sign_resolution[1]):
+                    line = get_line(frame[abs_y + y])
+                    keyboard.write(line[abs_x:(abs_x+sign_resolution[0])])
+                    if y < sign_resolution[1] - 1:
+                        keyboard.press("enter")
+
+                # Exit screen and wait for game to update
+                keyboard.press("esc")
+                time.sleep(delay)
+
+        if block_resolution != (1, 1):
+            move_mouse_relative(-last_movement[0], -last_movement[1])
+            time.sleep(delay)
 
         # Convert frame count to string
         viewed_frame_str = pad_number(viewed_frame, frame_padding)
